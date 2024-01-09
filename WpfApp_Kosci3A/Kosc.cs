bc9a5001f1ecccfc44071c3356b81531d837fb69
markup54 <marzenakupka@tlen.pl>
2024-01-09 12:12:15 +0100

    rzut koscia

---
+++ WpfApp_Kosci3A/Kosc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp_Kosci3A
+{
+    public class Kosc
+    {
+        public int Wartosc { get; set; }
+        public BitmapImage BitmapImage { get; set; }
+        public bool Zablokowana { get; set; }
+
+        public List<BitmapImage> BitmapImages { get; set; }
+
+        private void przygotujObrazki()
+        {
+         
+            BitmapImages = new List<BitmapImage>();
+            BitmapImages.Add(new BitmapImage(new Uri("pusta.png",UriKind.Relative)));
+           
+            BitmapImages.Add(new BitmapImage(new Uri("kosc1.png",UriKind.Relative)));
+            BitmapImages.Add(new BitmapImage(new Uri("kosc2.png",UriKind.Relative)));
+            BitmapImages.Add(new BitmapImage(new Uri("kosc3.png",UriKind.Relative)));
+            BitmapImages.Add(new BitmapImage(new Uri("kosc4.png",UriKind.Relative)));
+            BitmapImages.Add(new BitmapImage(new Uri("kosc5.png", UriKind.Relative)));
+            BitmapImages.Add(new BitmapImage(new Uri("kosc6.png", UriKind.Relative)));
+        }
+
+        public Kosc()
+        {
+            przygotujObrazki();
+            Wartosc = 0;
+            BitmapImage = BitmapImages[0];
+            Zablokowana = false;
+        }
+
+        public void RzucKoscia()
+        {
+            if (Zablokowana == false)
+            {
+                Random random = new Random();
+                Wartosc = random.Next(1, 7);
+                BitmapImage = BitmapImages[Wartosc];
+            }
+        }
+    }
+}
--- WpfApp_Kosci3A/MainWindow.xaml.cs
+++ WpfApp_Kosci3A/MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace WpfApp_Kosci3A
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Image> imagesKosci { get; set; }
+        List<Kosc>  Kosci { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            przygotujObrazy();
+            przygotujKosci();
+        }
+
+        private void przygotujObrazy()
+        {
+            imagesKosci = new List<Image>();
+            imagesKosci.Add(zdj1);
+            imagesKosci.Add(zdj2);
+            imagesKosci.Add(zdj3);
+            imagesKosci.Add(zdj4);
+            imagesKosci.Add(zdj5);
+        }
+        private void przygotujKosci()
+        {
+            Kosci = new List<Kosc>();
+            for(int i = 0;i<5;i++)
+            {
+                Kosci.Add(new Kosc());
+            }
+        }
+
+        private void Button_Rzut_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i =0;i<Kosci.Count;i++) 
+            {
+                Kosci[i].RzucKoscia();
+                imagesKosci[i].Source = Kosci[i].BitmapImage;
+            }
+            
         }
     }
 }
